db56ee502f4748dd65cd88152cde5b972cd5aad6
ThulaniMhlongo <mpilom96@gmail.com>
2015-10-05 16:18:05 +0200

    Homework

--- mm_alloc.h
+++ mm_alloc.h
@@ -48,7 +48,7 @@ s_block_ptr get_block (void *p);
 /* Add a new block at the of heap,
  * return NULL if things go wrong
  */
-s_block_ptr extend_heap (s_block_ptr last , size_t s);
+s_block_ptr extend_heap (size_t s);
 
 
 #ifdef __cplusplus
